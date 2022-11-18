c972f9e1bc34ad85c62b0de4d03e6e1e0348fd50
윤희찬 <yhc920923@gmail.com>
2022-11-18 20:13:27 +0900

    data binding

---
+++ src/views/1_databinding/DataBindingHtmlView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+      htmlString: ''
+    }
+  },
+  setup () {
+  },
+  created () {
+  },
+  mounted () {
+  },
+  unmounted () {
+  },
+  methods () {
+  }
+}
+</script>
---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="text-red">Hello {{ userName }}</h1>
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataBindingStringView.vue',
+  data () {
+    return {
+      userName: 'John Doe',
+      message: 'Welcome',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>

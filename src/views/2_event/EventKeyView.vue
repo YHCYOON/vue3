256c6409586238b398b6bff9a61e6c5f75b6c55a
윤희찬 <yhc920923@gmail.com>
2022-11-22 19:50:40 +0900

    event

---
+++ src/views/2_event/EventKeyView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+      sampleData: ''
+    }
+  },
+  setup () {
+  },
+  created () {
+  },
+  mounted () {
+  },
+  unmounted () {
+  },
+  methods: {}
+}
+</script>
+<style scoped>
+
+</style>
